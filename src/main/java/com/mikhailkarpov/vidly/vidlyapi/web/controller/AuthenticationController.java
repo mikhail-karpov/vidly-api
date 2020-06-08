4dcfb4758012b23f269c37a389fc512b730211dc
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-06-08 23:54:50 +0200

    Refactoring

--- src/main/java/com/mikhailkarpov/vidly/vidlyapi/web/controller/AuthenticationController.java
+++ src/main/java/com/mikhailkarpov/vidly/vidlyapi/web/controller/AuthenticationController.java
@@ -1,31 +1,34 @@
 package com.mikhailkarpov.vidly.vidlyapi.web.controller;
 
 import com.mikhailkarpov.vidly.vidlyapi.security.JwtService;
-import com.mikhailkarpov.vidly.vidlyapi.service.UserService;
 import com.mikhailkarpov.vidly.vidlyapi.web.dto.AuthenticationRequest;
-import com.mikhailkarpov.vidly.vidlyapi.web.dto.AuthenticationResponse;
-import com.mikhailkarpov.vidly.vidlyapi.web.dto.UserDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthenticationController {
 
     private AuthenticationManager authenticationManager;
-    private UserService userService;
+    private UserDetailsService userDetailsService;
     private JwtService jwtService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, UserService userService, JwtService jwtService) {
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    UserDetailsService userDetailsService,
+                                    JwtService jwtService) {
         this.authenticationManager = authenticationManager;
-        this.userService = userService;
+        this.userDetailsService = userDetailsService;
         this.jwtService = jwtService;
     }
 
@@ -35,16 +38,13 @@ public class AuthenticationController {
             String email = request.getEmail();
             String password = request.getPassword();
 
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-            UserDto user = userService
-                    .findByEmail(email)
-                    .orElseThrow(() -> {
-                        String errMsg = String.format("User with email %s not found", email);
-                        return new UsernameNotFoundException(errMsg);
-            });
+            UserDetails user = userDetailsService.loadUserByUsername(email);
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password, user.getAuthorities()));
 
-            String jwt = jwtService.generateToken(user.getEmail());
-            AuthenticationResponse response = new AuthenticationResponse(jwt, user);
+            String jwt = jwtService.generateToken(email);
+            Map<String, String> response = new HashMap<>();
+            response.put("jwt", jwt);
+            response.put("username", email);
 
             return ResponseEntity.ok().body(response);
 
