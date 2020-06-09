9bfdcd368de9aeaee0195bd29106646a6042276f
MiKarpov <mikhailkarpov@hotmail.com>
2020-06-09 16:15:09 +0300

    fixed endpoints

--- src/main/java/com/mikhailkarpov/vidly/vidlyapi/web/controller/MovieController.java
+++ src/main/java/com/mikhailkarpov/vidly/vidlyapi/web/controller/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/movies")
+@RequestMapping("movies")
 @Slf4j
 public class MovieController {
 
